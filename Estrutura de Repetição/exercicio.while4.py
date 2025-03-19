5d26c201144e03c893e57fcbcaece1bd265e56b0
Bruno Kayky <85810462+brunokayky23@users.noreply.github.com>
2025-03-19 19:47:57 +0000

    Salvando

--- Estrutura de Repetição/exercicio.while4.py
+++ Estrutura de Repetição/exercicio.while4.py
@@ -1,3 +1,23 @@
 import os
 os.system("cls || clear")
 
+
+login_cadastrado = "marta"
+senha_cadastrada = "123"
+contador = 0
+
+
+while True:
+    login = input("Digite seu login: ")
+    senha = input("Digite sua senha: ")
+
+    if login_cadastrado == login and senha_cadastrada == senha:        
+        print("Acesso permitido")
+        break  
+    else: 
+        print("Acesso negado")
+        contador += 1
+
+    if contador == 3:
+        print("Número de tentativas excedido.")
+        break
